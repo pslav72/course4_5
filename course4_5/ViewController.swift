e1b2d5a6e9c84dcd728efe934686be1cd72a37fa
Polyakov Slava <pslav72@gmail.com>
2021-10-09 20:55:21 +0500

    first commit

---
+++ course4_5/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  course4_5
+//
+//  Created by Вячеслав Поляков on 09.10.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
